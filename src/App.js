44b2a46e9e126f157b962fc13bbbe58b719bc65b
Liberontissauri <jp.liberon@gmail.com>
2021-06-23 15:44:20 +0100

    added the weatherDisplay Component to the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import WeatherDisplay from './components/weatherDisplay/weatherDisplay.js';
+
+let degrees = 28
+let day = "23"
+let month = "06"
 
 function App() {
   return (
     <div className="App bg">
-
+      <WeatherDisplay weather="Snow" degrees={degrees} day={day} month={month} desc="Light clouds"></WeatherDisplay>
     </div>
   );
 }
